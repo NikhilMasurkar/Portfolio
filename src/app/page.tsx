5542517da87151739ef5be238f952ea10cbb5d5a
nikhil <nikhil@indigolearn.com>
2026-08-13 14:51:18 +0530

    feat: add worked-with row and stats strip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import { Hero } from "@/components/home/hero";
+import { ProofStrip } from "@/components/home/proof-strip";
 
 export default function HomePage() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <ProofStrip />
+    </>
+  );
 }
